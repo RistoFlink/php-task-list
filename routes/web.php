ad2b6d0c5725e153226f4ba467c2b5cc471f0f9c
RistoFlink <kenssi@gmail.com>
2024-06-02 22:26:50 +0300

    Initial commit & play around with routing

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return 'Main Page';
+});
+
+Route::get('/hello', function () {
+    return 'Hello';
+})->name('hello');
+
+Route::get('/hallo', function () {
+    return redirect()->route('hello');
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello ' . $name . '!';
+});
+
+Route::fallback(function () {
+    return 'Still got somewhere!';
+});
