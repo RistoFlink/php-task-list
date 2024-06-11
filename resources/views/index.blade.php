09ffeaf8e220195e534c855e999b03b94786615c
RistoFlink <kenssi@gmail.com>
2024-06-11 18:55:26 +0300

    Modify layouts to use template inheritance

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,15 @@
-<h1>
-    Task list
-</h1>
+@extends('layouts.app')
 
-<div>
-    <!-- @if (count($tasks) > 0) -->
-    @forelse ($tasks as $task)
-        <div>
-            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
-        </div>
-    @empty
-        <div>No tasks exist!</div>
-    @endforelse
-    <!-- @endif -->
-</div>
+@section('title', 'Task list')
+
+@section('content')
+<!-- @if (count($tasks) > 0) -->
+@forelse ($tasks as $task)
+    <div>
+        <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+    </div>
+@empty
+    <div>No tasks exist!</div>
+@endforelse
+<!-- @endif -->
+@endsection
